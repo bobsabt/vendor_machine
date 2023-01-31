c61a205e74fdf022027d8b00db9d9d0f21c950f4
bobsabt <bodai.barbara@gmail.com>
2023-01-31 18:04:38 +0100

    add package

--- src/theme/GlobalStyles.ts
+++ src/theme/GlobalStyles.ts
@@ -1,50 +1,45 @@
-import { css, Global } from '@emotion/react'
 import { Theme } from '@mui/material'
+import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyles = () => {
-   // return <Global styles={theme => globalStyle(theme)} />
+export const GlobalStyles = (theme: Theme) => createGlobalStyle`
+        *,
+        *:before,
+        *:after {
+            box-sizing: border-box;
+        }
+        html {
+            overflow-y: scroll;
+        }
+        html,
+        body,
+        #__next {
+            height: 100%;
+        }
+        @font-face {
+            font-family: 'Gobold';
+            src: url('/fonts/goboldreg.otf') format('otf');
+            font-style: normal;
+            font-weight: 400;
+            font-display: swap;
+        }
+        a {
+            color: ${theme.palette.primary.main};
+        }
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+            margin: 0;
+            padding: 0;
+            color: ${theme.palette.primary.main};
+        }
 
-}
-
-const globalStyle = (theme: Theme) => css`
-    *,
-    *:before,
-    *:after {
-        box-sizing: border-box;
-    }
-    html {
-        overflow-y: scroll;
-    }
-    html,
-    body,
-    #__next {
-        height: 100%;
-    }
-    @font-face {
-        font-family: 'Gobold';
-        src: url('/fonts/goboldreg.otf') format('otf');
-        font-style: normal;
-        font-weight: 400;
-        font-display: swap;
-    }
-    a {
-        color: ${theme.palette.primary.main};
-    }
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6 {
-        margin: 0;
-        padding: 0;
-        color: ${theme.palette.primary.main};
-    }
-
-    body {
-        background-color: ${theme.palette.primary.main}
-    }
-    :root {
-        color-scheme: dark;
-    }
+        body {
+            background-color: ${theme.palette.primary.main}
+        }
+        :root {
+            color-scheme: dark;
+        }
 `
